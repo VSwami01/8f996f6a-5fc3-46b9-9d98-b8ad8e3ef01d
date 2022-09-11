fb1236ebd77139f4b2be2ac9eac86096097ec48f
vaibhavswami <vaibhavswami@hotmail.com>
2022-09-11 14:33:10 +1000

    Add generic extention to string.

--- LongestIncSubsequence/Extentions/StringExtention.cs
+++ LongestIncSubsequence/Extentions/StringExtention.cs
@@ -1,6 +1,4 @@
-﻿using System.Text.RegularExpressions;
-
-namespace LongestIncSubsequence.Extentions
+﻿namespace LongestIncSubsequence.Extentions
 {
     public static class StringExtention
     {
@@ -18,12 +16,42 @@ namespace LongestIncSubsequence.Extentions
         // Exceptions:
         //   T:System.ArgumentNullException:
         //     string is null.
+        //   T:System.FormatException:
+        //     string not in correct format.
+        //   T:System.InvalidCastException:
+        //     conversion not supported.
 
         public static int[] ToIntArray(this string input, char delimiter)
         {
+            return input.ToArray<int>(delimiter);
+        }
+
+        //
+        // Summary:
+        //     Converts a string to an array using a delimeter.
+        //
+        // Parameters:
+        //   string:
+        //     String having delimited values to convert to an array of provided type.
+        //
+        // Returns:
+        //     An array of provided type from the source string.
+        //
+        // Exceptions:
+        //   T:System.ArgumentNullException:
+        //     string is null.
+        //   T:System.FormatException:
+        //     string not in correct format.
+        //   T:System.InvalidCastException:
+        //     conversion not supported.
+        public static T[] ToArray<T>(this string input, char delimiter) where T : IConvertible
+        {
             var inputNumString = input.Split(delimiter);
 
-            return Array.ConvertAll(inputNumString, int.Parse);
+            return Array.ConvertAll(inputNumString, (inputStr) =>
+            {
+                return (T)Convert.ChangeType(inputStr, typeof(T));
+            });
         }
     }
 }
